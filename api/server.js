3e71d2f5ec735534b3ddd540c3db629261022a7e
miguelamoreira <40210256@esmad.ipp.pt>
2024-12-30 22:11:55 +0000

    Initial commit with api and app folders

---
+++ api/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+const db = require("./models");
+
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.status(200).json({ message: "home -- Seasoned api" });
+});
+
+app.listen(port, () => console.log(`App listening at http://${host}:${port}/`));
