f891e46ce5119677b7bc8efe3d152066f09ede4b
miguelamoreira <40210256@esmad.ipp.pt>
2024-12-31 20:03:01 +0000

    feat: badges page

---
+++ app/app/users/[userId]/badges.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, FlatList } from 'react-native';
+import { useRouter, useLocalSearchParams } from 'expo-router';
+import * as Progress from 'react-native-progress';
+
+import OptionsTab from '@/components/OptionsTab'; 
+import Badge from '@/components/Badge';
+import { fetchBadges } from '@/api/badgesApi';
+
+interface BadgeData {
+    image: string;
+    title: string;
+    description: string;
+    howTo: string;
+    progress: number | null;
+    earned: boolean;
+    id: number; 
+}
+
+export default function BadgesScreen() {
+    const { userId } = useLocalSearchParams<{ userId: string }>();
+    const router = useRouter();
+    const [badges, setBadges] = useState<BadgeData[]>([]);
+
+    const sortedBadges = badges.sort((a, b) => Number(a.earned) - Number(b.earned)); 
+
+    useEffect(() => {
+        if (userId) {
+            fetchBadges(parseInt(userId))
+                .then(setBadges)
+                .catch((err) => console.error('Error fetching badges:', err));
+        }
+    }, [userId]);    
+
+    return (
+        <SafeAreaView style={styles.mainContainer}>
+            <OptionsTab type="back" onBackPress={() => router.back()} />
+
+            <Text style={styles.heading}>Badges</Text>
+
+            <Progress.Bar
+                progress={50 / 100}
+                width={378}
+                color="#82AA59"
+                borderColor="#352A23"
+                unfilledColor="#352A23"
+                style={{ marginTop: 12, marginBottom: 16 }}
+            />
+
+            <FlatList
+                data={sortedBadges} 
+                keyExtractor={(item, index) => item.id ? item.id.toString() : index.toString()}
+                numColumns={2} 
+                contentContainerStyle={styles.badgeGrid} 
+                renderItem={({ item }) => (
+                    <View style={styles.badgeWrapper}>
+                        <Badge variant="grid" image={item.image} title={item.title} earned={item.earned} onPress={() => router.push(`/users/${userId}/badges/${item.id}`)}/>
+                    </View>
+                )}
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#FFF4E0',
+        paddingHorizontal: 16,
+        paddingTop: 42,
+    },
+    heading: {
+        fontSize: 24,
+        fontFamily: 'DMSerifText',
+        lineHeight: 45,
+        color: '#352A23',
+    },
+    badgeGrid: {
+        paddingBottom: 16, 
+    },
+    badgeWrapper: {
+        width: '48%', 
+        alignItems: 'center',
+        marginVertical: 10,
+        marginHorizontal: '1%',
+    },
+});
